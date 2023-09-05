f108ed9db377e336ba7debcf307b854ad51cd397
dmitry <shestaevd26@gmail.com>
2023-09-06 00:31:22 +0300

    GodotBeatSystem

---
+++ scripts/BeatSystem/BeatSystemGodot/Track/GodotTrack.cs
@@ -0,0 +1,21 @@
+﻿using BeatSystem.scripts.BeatSystem.Domain.Properties;
+using Godot;
+using System;
+
+namespace TileBeat.scripts.BeatSystem.BeatSystemGodot.Track
+{
+    internal record GodotTrack(AudioStream audioStream, int bpm) : HasBpm, HasFullLength
+    {
+        private double fullLength = audioStream.GetLength();
+
+        public int GetBpm()
+        {
+            return bpm;
+        }
+
+        public double GetFullLength()
+        {
+            return fullLength;
+        }
+    }
+}
