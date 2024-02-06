5a02368ef4c3f9f115b45c84122b1bb6641650bb
dmitry <shestaevd26@gmail.com>
2024-02-06 14:58:28 +0300

    aaa

---
+++ scripts/GameObjects/Tiles/Tile.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileBeat.scripts.GameObjects.Tiles
+{
+    public abstract record AbstractTile(uint GridX, uint GridY);
+    public record EmptyTile(uint GridX, uint GridY) : AbstractTile(GridX, GridY);
+    public record Tile(uint GridX, uint GridY, Texture2D Texture, Vector2 spriteSize, ITileSpawner Spawner) : AbstractTile(GridX, GridY)
+    {
+        public void Spawn(Vector2 position)
+        {
+            Spawner.Spawn(position, Texture);
+        }
+    }
+
+}
