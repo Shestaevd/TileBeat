3bc3e9afe944d452e75c66eb42f65172a9dc330c
dmitry <shestaevd26@gmail.com>
2024-02-06 14:51:49 +0300

    aaa

--- scripts/GameObjects/Tiles/ITileSpawner.cs
+++ scripts/GameObjects/Tiles/ITileSpawner.cs
@@ -4,6 +4,6 @@ namespace TileBeat.scripts.GameObjects.Tiles
 {
     public interface ITileSpawner
     {
-        public void Spawn(Vector2 spawnPosition, Texture2D sprite);
+        public void Spawn(Vector2 spawnPosition, Texture2D sprite, Vector2 targetSize);
     }
 }
